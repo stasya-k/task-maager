a540492213e21873fdfbf7f9de012f521258a7e4
Anastasia <anastasia.zhura@gmail.com>
2021-11-19 16:36:01 +0300

    add tests

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,10 +1,19 @@
 FactoryBot.define do
   factory :user do
-    first_name { "MyString" }
-    last_name { "MyString" }
-    password { "" }
-    email { "MyString" }
+    first_name
+    last_name
+    password
+    email
     avatar { "MyString" }
     type { "" }
+    factory :developer do
+      type { "Developer" }
+    end
+    factory :manager do
+      type { "Manager" }
+    end
+    factory :admin do
+      type { "Admin"}
+    end
   end
 end
