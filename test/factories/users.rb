08c4cc6d452d8261606f5ee713e3a6c7b35ac705
Anastasia <anastasia.zhura@gmail.com>
2021-11-19 16:37:38 +0300

    fix linter issues

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -4,16 +4,16 @@ FactoryBot.define do
     last_name
     password
     email
-    avatar { "MyString" }
-    type { "" }
+    avatar { 'MyString' }
+    type { '' }
     factory :developer do
-      type { "Developer" }
+      type { 'Developer' }
     end
     factory :manager do
-      type { "Manager" }
+      type { 'Manager' }
     end
     factory :admin do
-      type { "Admin"}
+      type { 'Admin' }
     end
   end
 end
