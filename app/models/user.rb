03468b05a2c561f5e3ee48694ec3a56dc97d42ce
Anastasia <anastasia.zhura@gmail.com>
2021-11-22 22:23:22 +0300

    add validations for user and task

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true , format: /@/
 end
