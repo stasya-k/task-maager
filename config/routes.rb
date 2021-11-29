504f05b3577c9e176dd3cf33a0e56869847d6669
Anastasia <anastasia.zhura@gmail.com>
2021-11-29 13:01:45 +0000

    add user page

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
     resource :session, only: [:new, :create, :destroy]
     resources :developers, only: [:new, :create]
   end
+  namespace :admin do
+    resources :users
+  end
 end
