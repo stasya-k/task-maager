47b051627ed670de77b42a397782982ddb44fd02
Anastasia <anastasia.zhura@gmail.com>
2022-01-27 14:12:58 +0300

    update TaskBoard

--- app/javascript/components/TaskBoard/TaskBoard.js
+++ app/javascript/components/TaskBoard/TaskBoard.js
@@ -1,36 +1,74 @@
-import React from 'react';
+/* eslint-disable react/function-component-definition */
+import React, { useEffect, useState } from 'react';
 import Board from '@asseinfo/react-kanban';
 import '@asseinfo/react-kanban/dist/styles.css';
+import { propOr } from 'ramda';
 
-const data = {
-  columns: [
-    {
-      id: 1,
-      title: 'Backlog',
-      cards: [
-        {
-          id: 1,
-          title: 'Add card',
-          description: 'Add capability to add a card in a column',
-        },
-      ],
-    },
-    {
-      id: 2,
-      title: 'Doing',
-      cards: [
-        {
-          id: 2,
-          title: 'Drag-n-drop support',
-          description: 'Move a card between the columns',
-        },
-      ],
-    },
-  ],
+import Task from 'components/Task';
+import TasksRepository from 'repositories/TasksRepository.js';
+
+const STATES = [
+  { key: 'new_task', value: 'New' },
+  { key: 'in_development', value: 'In Dev' },
+  { key: 'in_qa', value: 'In QA' },
+  { key: 'in_code_review', value: 'in CR' },
+  { key: 'ready_for_release', value: 'Ready for release' },
+  { key: 'released', value: 'Released' },
+  { key: 'archived', value: 'Archived' },
+];
+
+const initialBoard = {
+  columns: STATES.map((column) => ({
+    id: column.key,
+    title: column.value,
+    cards: [],
+    meta: {},
+  })),
 };
 
-const TaskBoard = function () {
-  return <Board disableColumnDrag initialBoard={data} />;
+const TaskBoard = () => {
+  const [board, setBoard] = useState(initialBoard);
+  const [boardCards, setBoardCards] = useState([]);
+
+  const loadColumn = (state, page, perPage) =>
+    TasksRepository.index({
+      q: { stateEq: state },
+      page,
+      perPage,
+    });
+
+  const loadColumnInitial = (state, page = 1, perPage = 10) => {
+    loadColumn(state, page, perPage).then(({ data }) => {
+      setBoardCards((prevState) => ({
+        ...prevState,
+        [state]: { cards: data.items, meta: data.meta },
+      }));
+    });
+  };
+
+  const generateBoard = () => {
+    setBoard({
+      columns: STATES.map(({ key, value }) => ({
+        id: key,
+        title: value,
+        cards: propOr({}, 'cards', boardCards[key]),
+        meta: propOr({}, 'meta', boardCards[key]),
+      })),
+    });
+  };
+
+  const loadBoard = () => {
+    STATES.map(({ key }) => loadColumnInitial(key));
+  };
+
+  useEffect(() => generateBoard(), [boardCards]);
+  useEffect(() => loadBoard(), []);
+
+  return (
+    <Board disableColumnDrag renderCard={(card) => <Task task={card} />}>
+      {board}
+    </Board>
+  );
 };
 
 export default TaskBoard;
